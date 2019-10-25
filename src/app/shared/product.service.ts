1f5a63a48a895ebcba68a11952db1fd804bf21d0
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-10-25 08:58:24 -0700

    Add product item component formatting

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -3,7 +3,47 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public description: string,
+    public categories: string[]) {}
+
+}
+
+const products = [
+  {
+    'id': 0,
+    'title': 'First Product',
+    'price': 24.99,
+    'rating': 4.3,
+    'description': 'This is short description',
+    'categories': ['electronics', 'hardware']
+  },
+  {
+    'id': 1,
+    'title': 'Second Product',
+    'price': 64.99,
+    'rating': 3.5,
+    'description': 'This is short description.',
+    'categories': ['books']
+  }
+];
+
+
 export class ProductService {
 
-  constructor() { }
+  getProducts(): Product[] {
+    return products.map(p => new Product(p.id, p.title, p.price, p.rating, p.description, p.categories));
+  }
+
+  getProductById(productId: number): Product {
+    return this.getProducts().filter(p => p.id === productId).shift();
+  }
+
 }
+
